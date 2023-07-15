5a9a9077a2b69c7a7adf03d58cc4a2b14fc4d261
Seth <gaurav@workforceanalytics.in>
2023-07-15 12:37:55 +0530

    re-structured for webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const config = require("./package.json");
+
+module.exports = {
+    entry: path.resolve(__dirname, config.main),  // entry file to all our js modules
+    devtool: "source-map",  // add source mapping
+    output: {
+        path: __dirname +'/public',  // path to output files
+        filename: 'dist/bundle.js'
+    },
+    // optimization: {
+    //     minimizer: [new UglifyJsPlugin()],
+    // },
+    mode: 'development'
+}
